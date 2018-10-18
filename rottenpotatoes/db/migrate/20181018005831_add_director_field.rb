a74bbdcc2a36e79e1867050f7fbb6c412192a992
Christopher Jung <christopherjung@sandiego.edu>
2018-10-18 01:26:02 +0000

    Lets try this again

--- rottenpotatoes/db/migrate/20181018005831_add_director_field.rb
+++ rottenpotatoes/db/migrate/20181018005831_add_director_field.rb
@@ -1,5 +1,6 @@
 class AddDirectorField < ActiveRecord::Migration
   def change
+    remove_column :movies, :director
     add_column :movies, :director, :string
   end
 end
--- rottenpotatoes/spec/rails_helper.rb
+++ rottenpotatoes/spec/rails_helper.rb
@@ -3,6 +3,8 @@ require 'simplecov'
 SimpleCov.start 'rails'
 require 'spec_helper'
 
+
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
